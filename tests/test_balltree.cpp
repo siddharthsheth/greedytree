5dddbb7ae5667ec763905601e44e658cedf96b93
Siddharth Sheth <siddharthsheth@MacBookAir.lan>
2025-09-07 16:17:34 -0400

    cleaned up balltree unit test

--- tests/test_balltree.cpp
+++ tests/test_balltree.cpp
@@ -4,13 +4,9 @@
 
 TEST(BallTreeTest, LeafInitialization) {
     using MyPoint = Point<3, L2Metric>;
-    MyPoint pt(std::initializer_list<double>{1.0, 2.0, 3.0});
-    // MyPoint pt(std::initializer_list<double>{1.0, 2.0, 3.0});
-    BallTree<3, L2Metric> tree(&pt);
 
-    // std::vector<std::unique_ptr<const MyPoint>> inp, gp, pred;
-    // inp.push_back(std::move(pt));
-    // clarkson(inp, gp, pred);
+    MyPoint pt(std::initializer_list<double>{1.0, 2.0, 3.0});
+    BallTree<3, L2Metric> tree(&pt);
 
     EXPECT_TRUE(tree.isleaf());
     EXPECT_EQ(tree._size, 1);
